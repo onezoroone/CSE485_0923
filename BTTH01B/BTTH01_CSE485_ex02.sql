58f2f16657879ffb7b6a7828e284dd228420bc79
onezoroone <85349645+onezoroone@users.noreply.github.com>
2023-09-19 13:00:53 +0700

    Update BTTH01_CSE485_ex02.sql

--- BTTH01B/BTTH01_CSE485_ex02.sql
+++ BTTH01B/BTTH01_CSE485_ex02.sql
@@ -64,3 +64,69 @@ SELECT ma_bviet, tieude, ten_bhat
 FROM baiviet
 WHERE tieude LIKE '%yêu%' OR tieude LIKE '%thương%' OR tieude LIKE '%anh%' OR tieude LIKE '%em%' OR 
 ten_bhat LIKE '%yêu%' OR ten_bhat LIKE '%thương%' OR ten_bhat LIKE '%anh%' OR ten_bhat LIKE '%em%';
+
+--i.
+CREATE VIEW vw_Music AS
+SELECT
+    baiviet.ma_bviet AS MaBaiViet,
+    baiviet.tieude AS TieuDeBaiViet,
+    baiviet.ten_bhat AS TenBaiHat,
+    theloai.ten_tloai AS TenTheLoai,
+    tacgia.ten_tgia AS TenTacGia
+FROM baiviet INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai
+INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia;
+
+--j.
+DELIMITER //
+CREATE PROCEDURE sp_DSBaiViet(IN p_ten_tloai VARCHAR(50))
+BEGIN
+    DECLARE v_ma_tloai INT;
+    SELECT ma_tloai INTO v_ma_tloai FROM theloai WHERE ten_tloai = p_ten_tloai;
+    IF v_ma_tloai IS NULL THEN
+        SELECT 'Thể loại không tồn tại' AS KQ;
+    ELSE
+        SELECT baiviet.ma_bviet AS MaBaiViet, baiviet.tieude AS TieuDeBaiViet, baiviet.ten_bhat AS TenBaiHat
+        FROM baiviet
+        WHERE baiviet.ma_tloai = v_ma_tloai;
+    END IF;
+END //
+
+DELIMITER ;
+
+--k.
+ALTER TABLE theloai
+ADD COLUMN SLBaiViet INT UNSIGNED DEFAULT 0;
+
+UPDATE theloai AS tl
+SET SLBaiViet = (
+    SELECT COUNT(*)
+    FROM baiviet AS bv
+    WHERE bv.ma_tloai = tl.ma_tloai
+);
+
+DELIMITER //
+CREATE TRIGGER tg_CapNhatTheLoai
+AFTER INSERT ON baiviet
+FOR EACH ROW
+BEGIN
+    UPDATE theloai
+    SET SLBaiViet = SLBaiViet + 1
+    WHERE ma_tloai = NEW.ma_tloai;
+END;
+
+CREATE TRIGGER tg_XoaBaiViet
+AFTER DELETE ON baiviet
+FOR EACH ROW
+BEGIN
+    UPDATE theloai
+    SET SLBaiViet = SLBaiViet - 1
+    WHERE ma_tloai = OLD.ma_tloai;
+END;
+DELIMITER ;
+
+--l.
+CREATE TABLE users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(50) NOT NULL UNIQUE,
+	PASSWORD VARCHAR(100) NOT NULL
+)
