4ced0bec66e9c738295012c6b930a90cce04d53a
onezoroone <85349645+onezoroone@users.noreply.github.com>
2023-09-19 08:09:32 +0700

    Add files via upload

---
+++ bai3.php
@@ -0,0 +1,9 @@
+<?php
+    $arrs=['PHP','HTML','CSS','JS'];
+    echo '<table border="2">';
+    echo '<tr><td>Tên khóa học</td></tr>';
+    echo'<tr><td>'.$arrs[0].'</td></tr>';
+    echo'<tr><td>'.$arrs[1].'</td></tr>';
+    echo'<tr><td>'.$arrs[2].'</td></tr>';
+    echo'<tr><td>'.$arrs[3].'</td></tr>';
+    echo'</table>';
