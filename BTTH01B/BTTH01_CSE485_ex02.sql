8f1cbc491847c52ee699efa5c53a59ea3e570303
onezoroone <85349645+onezoroone@users.noreply.github.com>
2023-09-21 15:57:11 +0700

    Update.sql

--- BTTH01B/BTTH01_CSE485_ex02.sql
+++ BTTH01B/BTTH01_CSE485_ex02.sql
@@ -21,51 +21,51 @@ CREATE TABLE baiviet(
 	FOREIGN KEY(ma_tloai) REFERENCES theloai(ma_tloai),
 	FOREIGN KEY(ma_tgia) REFERENCES tacgia(ma_tgia)
 )
---a.
+--a.Liệt kê các bài viết về các bài hát thuộc thể loại Nhạc trữ tình
 SELECT
     ma_bviet, tieude, ten_bhat, ma_tgia, ten_tloai, ngayviet
 FROM baiviet INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai
 WHERE ten_tloai = 'Nhạc trữ tình';
 
---b.
+--b.Liệt kê các bài viết của tác giả “Nhacvietplus” 
 SELECT
     ma_bviet, tieude, ten_bhat, ten_tgia, ma_tloai, ngayviet
 FROM baiviet INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia
 WHERE ten_tgia = 'Nhacvietplus';
 
---c.
+--c.Liệt kê các thể loại nhạc chưa có bài viết cảm nhận nào.
 SELECT ten_tloai
 FROM theloai LEFT JOIN baiviet ON theloai.ma_tloai = baiviet.ma_tloai
 WHERE baiviet.ma_bviet IS NULL;
 
---d.
+--d.Liệt kê các bài viết với các thông tin sau: mã bài viết, tên bài viết, tên bài hát, tên tác giả, tên thể loại, ngày viết.
 SELECT baiviet.ma_bviet, baiviet.tieude, baiviet.ten_bhat, tacgia.ten_tgia, theloai.ten_tloai, baiviet.ngayviet
 FROM baiviet INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai;
 
---e.
+--e.Tìm thể loại có số bài viết nhiều nhất
 SELECT theloai.ten_tloai, COUNT(*) AS so_bai_viet FROM baiviet
 INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai
 GROUP BY theloai.ten_tloai
 ORDER BY so_bai_viet DESC LIMIT 2;
 
---f.
+--f.Liệt kê 2 tác giả có số bài viết nhiều nhất
 SELECT tacgia.ten_tgia, COUNT(*) AS so_bai_viet
 FROM baiviet INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia
 GROUP BY tacgia.ten_tgia
 ORDER BY so_bai_viet DESC LIMIT 2;
 
---g.
+--g.Liệt kê các bài viết về các bài hát có tựa bài hát chứa 1 trong các từ “yêu”, “thương”, “anh”, “em”
 SELECT ma_bviet, tieude, ten_bhat
 FROM baiviet
 WHERE ten_bhat LIKE '%yêu%' OR ten_bhat LIKE '%thương%' OR ten_bhat LIKE '%anh%' OR ten_bhat LIKE '%em%';
 
---h.
+--h.Liệt kê các bài viết về các bài hát có tiêu đề bài viết hoặc tựa bài hát chứa 1 trong các từ “yêu”, “thương”, “anh”, “em”
 SELECT ma_bviet, tieude, ten_bhat
 FROM baiviet
 WHERE tieude LIKE '%yêu%' OR tieude LIKE '%thương%' OR tieude LIKE '%anh%' OR tieude LIKE '%em%' OR 
 ten_bhat LIKE '%yêu%' OR ten_bhat LIKE '%thương%' OR ten_bhat LIKE '%anh%' OR ten_bhat LIKE '%em%';
 
---i.
+--i.Tạo 1 view có tên vw_Music để hiển thị thông tin về Danh sách các bài viết kèm theo Tên thể loại và tên tác giả
 CREATE VIEW vw_Music AS
 SELECT
     baiviet.ma_bviet AS MaBaiViet,
@@ -76,55 +76,7 @@ SELECT
 FROM baiviet INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai
 INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia;
 
---j.
-DELIMITER //
-CREATE PROCEDURE sp_DSBaiViet(IN p_ten_tloai VARCHAR(50))
-BEGIN
-    DECLARE v_ma_tloai INT;
-    SELECT ma_tloai INTO v_ma_tloai FROM theloai WHERE ten_tloai = p_ten_tloai;
-    IF v_ma_tloai IS NULL THEN
-        SELECT 'Thể loại không tồn tại' AS KQ;
-    ELSE
-        SELECT baiviet.ma_bviet AS MaBaiViet, baiviet.tieude AS TieuDeBaiViet, baiviet.ten_bhat AS TenBaiHat
-        FROM baiviet
-        WHERE baiviet.ma_tloai = v_ma_tloai;
-    END IF;
-END //
-
-DELIMITER ;
-
---k.
-ALTER TABLE theloai
-ADD COLUMN SLBaiViet INT UNSIGNED DEFAULT 0;
-
-UPDATE theloai AS tl
-SET SLBaiViet = (
-    SELECT COUNT(*)
-    FROM baiviet AS bv
-    WHERE bv.ma_tloai = tl.ma_tloai
-);
-
-DELIMITER //
-CREATE TRIGGER tg_CapNhatTheLoai
-AFTER INSERT ON baiviet
-FOR EACH ROW
-BEGIN
-    UPDATE theloai
-    SET SLBaiViet = SLBaiViet + 1
-    WHERE ma_tloai = NEW.ma_tloai;
-END;
-
-CREATE TRIGGER tg_XoaBaiViet
-AFTER DELETE ON baiviet
-FOR EACH ROW
-BEGIN
-    UPDATE theloai
-    SET SLBaiViet = SLBaiViet - 1
-    WHERE ma_tloai = OLD.ma_tloai;
-END;
-DELIMITER ;
-
---l.
+--l.Bổ sung thêm bảng Users để lưu thông tin Tài khoản đăng nhập và sử dụng cho chức năng Đăng nhập/Quản trị trang web.
 CREATE TABLE users(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(50) NOT NULL UNIQUE,
